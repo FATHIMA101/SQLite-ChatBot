6d821ac2d04c560bcf99904dc70e3a269b8ec9eb
FATHIMA101 <fathimakabeerm101@gmail.com>
2025-02-08 16:11:11 +0530

    Use dynamic port for Render

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ def index():
 
 # Start the Flask app
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+     port = int(os.environ.get("PORT", 10000))  # Use Render's assigned port
+     app.run(host="0.0.0.0", port=port)
