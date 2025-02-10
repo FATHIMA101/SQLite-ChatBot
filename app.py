667232c6579a1540100a3b377a4f5a0077c1efc1
FATHIMA101 <134140436+FATHIMA101@users.noreply.github.com>
2025-02-10 15:04:16 +0530

    app.py

--- app.py
+++ app.py
@@ -107,5 +107,5 @@ def index():
 
 # Start the Flask app
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 1000))  # Use Render's assigned port
+     port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
      app.run(host="0.0.0.0", port=port)
